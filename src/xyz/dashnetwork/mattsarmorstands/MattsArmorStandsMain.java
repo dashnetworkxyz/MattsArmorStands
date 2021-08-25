6e078e23cf66284bb609437776d87f89dc6e81f6
goldenobsidian5 <goldenobsidian5@gmail.com>
2021-08-25 01:14:35 -0400

    Initial commit

---
+++ src/xyz/dashnetwork/mattsarmorstands/MattsArmorStandsMain.java
@@ -0,0 +1,19 @@
+package xyz.dashnetwork.mattsarmorstands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import xyz.dashnetwork.mattsarmorstands.commands.CommandMattsArmorStands;
+
+public class MattsArmorStandsMain extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getCommand("mattsarmorstands").setExecutor(new CommandMattsArmorStands());
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
---
+++ src/xyz/dashnetwork/mattsarmorstands/commands/CommandMattsArmorStands.java
@@ -0,0 +1,12 @@
+package xyz.dashnetwork.mattsarmorstands.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandMattsArmorStands implements CommandExecutor {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        sender.sendMessage("§c§lMattsArmorStands §6§l>> §6Developed by MM5. Version §cv1.0");
+        return true;
+    }
+}
