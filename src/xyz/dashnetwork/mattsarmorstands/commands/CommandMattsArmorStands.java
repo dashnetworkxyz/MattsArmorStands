5f89526505cd3dfdf1b2eca79ea009c1b4dd90f2
MasterDash5 <andrewbell25@icloud.com>
2021-10-06 23:16:24 -0600

    small cleanup

--- src/xyz/dashnetwork/mattsarmorstands/commands/CommandMattsArmorStands.java
+++ src/xyz/dashnetwork/mattsarmorstands/commands/CommandMattsArmorStands.java
@@ -5,8 +5,12 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 public class CommandMattsArmorStands implements CommandExecutor {
+    
+    @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         sender.sendMessage("§c§lMattsArmorStands §6§l>> §6Developed by MM5. Version §cv1.0");
+        
         return true;
     }
+    
 }
